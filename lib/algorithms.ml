c54173a1f2b6a8756a67ee2f87401d0895b79ee0
Anil Madhavapeddy <anil@recoil.org>
2012-03-03 15:45:37 +0000

    whitespace

--- lib/algorithms.ml
+++ lib/algorithms.ml
@@ -21,152 +21,162 @@ exception Not_implemented
 
 module Cipher = struct
 
-    type t =
-    |TripleDES_CBC
-    |AES_256_CBC
-    |AES_192_CBC
-    |AES_128_CBC
-    |Arcfour
-    |None
+  type t =
+  | TripleDES_CBC
+  | AES_256_CBC
+  | AES_192_CBC
+  | AES_128_CBC
+  | Arcfour
+  | None
 
-    type mode =
-    | Encrypt
-    | Decrypt
+  type mode =
+  | Encrypt
+  | Decrypt
 
-    type info = { name: t; key_len: int; block_size: int }
+  type info = {
+    name: t;
+    key_len: int;
+    block_size: int 
+  }
 
-    type fn = Stream of Stream.stream_cipher | Block of Block.block_cipher
+  type fn =
+  | Stream of Stream.stream_cipher 
+  | Block of Block.block_cipher
 
-    exception Unknown of string
+  exception Unknown of string
 
-    let to_string = function
-    | TripleDES_CBC -> "3des-cbc"
-    | AES_256_CBC -> "aes256-cbc"
-    | AES_192_CBC -> "aes192-cbc"
-    | AES_128_CBC -> "aes128-cbc"
-    | Arcfour -> "arcfour"
-    | None -> "none"
+  let to_string = function
+  | TripleDES_CBC -> "3des-cbc"
+  | AES_256_CBC -> "aes256-cbc"
+  | AES_192_CBC -> "aes192-cbc"
+  | AES_128_CBC -> "aes128-cbc"
+  | Arcfour -> "arcfour"
+  | None -> "none"
 
-    let from_string = function
-    | "3des-cbc" -> TripleDES_CBC
-    | "aes256-cbc" -> AES_256_CBC
-    | "aes192-cbc" -> AES_192_CBC
-    | "aes128-cbc" -> AES_128_CBC
-    | "arcfour" -> Arcfour
-    | "none" -> None
-    | x -> raise (Unknown x)
+  let from_string = function
+  | "3des-cbc" -> TripleDES_CBC
+  | "aes256-cbc" -> AES_256_CBC
+  | "aes192-cbc" -> AES_192_CBC
+  | "aes128-cbc" -> AES_128_CBC
+  | "arcfour" -> Arcfour
+  | "none" -> None
+  | x -> raise (Unknown x)
 
-    let info = function
-    | TripleDES_CBC -> {name=TripleDES_CBC; key_len=24; block_size=8}
-    | AES_256_CBC -> {name=AES_256_CBC; key_len=32; block_size=16}
-    | AES_192_CBC -> {name=AES_192_CBC; key_len=24; block_size=16}
-    | AES_128_CBC -> {name=AES_128_CBC; key_len=16; block_size=16}
-    | Arcfour -> {name=Arcfour; key_len=16; block_size=8}
-    | None -> {name=None; key_len=0; block_size=0}
+  let info = function
+  | TripleDES_CBC -> {name=TripleDES_CBC; key_len=24; block_size=8}
+  | AES_256_CBC -> {name=AES_256_CBC; key_len=32; block_size=16}
+  | AES_192_CBC -> {name=AES_192_CBC; key_len=24; block_size=16}
+  | AES_128_CBC -> {name=AES_128_CBC; key_len=16; block_size=16}
+  | Arcfour -> {name=Arcfour; key_len=16; block_size=8}
+  | None -> {name=None; key_len=0; block_size=0}
 
-    (* Given an initialisation vector, key, algorithm and cipher direction,
-       return a wrapped Cryptokit transformation of the appropriate type *)
-    let fn iv key alg =
-        let null_stream = object
-            method transform = String.blit
-            method wipe = ()
-        end in
+  (* Given an initialisation vector, key, algorithm and cipher direction,
+     return a wrapped Cryptokit transformation of the appropriate type *)
+  let fn iv key alg =
+    let null_stream = object
+      method transform = String.blit
+      method wipe = ()
+    end in
     function
-    | Encrypt -> begin match alg with
-        | TripleDES_CBC ->
-            let t = new Block.triple_des_encrypt key in
-            Block (new Block.cbc_encrypt ~iv:iv t)
-        | AES_256_CBC | AES_192_CBC | AES_128_CBC ->
-            let t = new Block.aes_encrypt key in
-            Block (new Block.cbc_encrypt ~iv:iv t)
-        | Arcfour -> Stream (new Stream.arcfour key)
-        | None -> Stream null_stream
-        end
-    | Decrypt -> begin match alg with
-        | TripleDES_CBC ->
-            let t = new Block.triple_des_decrypt key in
-            Block (new Block.cbc_decrypt ~iv:iv t)
-        | AES_256_CBC | AES_192_CBC | AES_128_CBC ->
-            let t = new Block.aes_decrypt key in
-            Block (new Block.cbc_decrypt ~iv:iv t)
-        | Arcfour -> Stream (new Stream.arcfour key)
-        | None -> Stream null_stream
-        end
+    |Encrypt -> begin 
+       match alg with
+       |TripleDES_CBC ->
+          let t = new Block.triple_des_encrypt key in
+          Block (new Block.cbc_encrypt ~iv:iv t)
+       |AES_256_CBC | AES_192_CBC | AES_128_CBC ->
+          let t = new Block.aes_encrypt key in
+          Block (new Block.cbc_encrypt ~iv:iv t)
+       |Arcfour -> Stream (new Stream.arcfour key)
+       |None -> Stream null_stream
+    end
+    |Decrypt -> begin
+       match alg with
+       |TripleDES_CBC ->
+          let t = new Block.triple_des_decrypt key in
+          Block (new Block.cbc_decrypt ~iv:iv t)
+       |AES_256_CBC | AES_192_CBC | AES_128_CBC ->
+          let t = new Block.aes_decrypt key in
+          Block (new Block.cbc_decrypt ~iv:iv t)
+       |Arcfour -> Stream (new Stream.arcfour key)
+       |None -> Stream null_stream
+    end
 
-    (* Accept a wrapper Cryptokit object and process the given data
-       through it.  Data must meet blocksize requirements *)
-    let data cipher srcbuf srcoff dstbuf dstoff len =
-        match cipher with 
-        | Block y ->
-            let bs = y#blocksize in
-            let num_blocks = (len / bs) - 1 in
-            assert (num_blocks >= 0);
-            for i = 0 to num_blocks do
-                y#transform srcbuf (srcoff+(i*bs)) dstbuf (dstoff+(i*bs))
-            done;
-        | Stream y ->
-            y#transform srcbuf srcoff dstbuf dstoff len
+  (* Accept a wrapper Cryptokit object and process the given data
+     through it.  Data must meet blocksize requirements *)
+  let data cipher srcbuf srcoff dstbuf dstoff len =
+    match cipher with 
+    |Block y ->
+      let bs = y#blocksize in
+      let num_blocks = (len / bs) - 1 in
+      assert (num_blocks >= 0);
+      for i = 0 to num_blocks do
+        y#transform srcbuf (srcoff+(i*bs)) dstbuf (dstoff+(i*bs))
+      done;
+    |Stream y ->
+      y#transform srcbuf srcoff dstbuf dstoff len
 end
 
 module MAC = struct
-    type t =
-    | SHA1
-    | SHA1_96
-    | MD5
-    | MD5_96
-    | None
+  type t =
+  | SHA1
+  | SHA1_96
+  | MD5
+  | MD5_96
+  | None
 
-    type info = { name: t; digest_len: int; key_len: int; fn: string->hash }
-        
-    exception Unknown of string
+  type info = { name: t; digest_len: int; key_len: int; fn: string->hash }
     
-    let to_string = function
-    | SHA1 -> "hmac-sha1"
-    | SHA1_96 -> "hmac-sha1-96"
-    | MD5 -> "hmac-md5"
-    | MD5_96 -> "hmac-md5-96"
-    | None -> "none"
-    
-    let from_string = function
-    | "hmac-sha1" -> SHA1
-    | "hmac-sha-96" -> SHA1_96
-    | "hmac-md5" -> MD5
-    | "hmac-md5-96" -> MD5_96
-    | "none" -> None
-    | x -> raise (Unknown x)
+  exception Unknown of string
+  
+  let to_string = function
+  | SHA1 -> "hmac-sha1"
+  | SHA1_96 -> "hmac-sha1-96"
+  | MD5 -> "hmac-md5"
+  | MD5_96 -> "hmac-md5-96"
+  | None -> "none"
+  
+  let from_string = function
+  | "hmac-sha1" -> SHA1
+  | "hmac-sha-96" -> SHA1_96
+  | "hmac-md5" -> MD5
+  | "hmac-md5-96" -> MD5_96
+  | "none" -> None
+  | x -> raise (Unknown x)
    
-    let null_mac (_:string) = object
-        method add_byte (x:int) = ()
-        method add_char (x:char) = ()
-        method add_string (x:string) = ()
-        method add_substring (x:string) (_:int) (_:int) = ()
-        method hash_size = 0
-        method result = ""
-        method wipe = () end
-    let info = function
-    | SHA1 -> { name=SHA1; digest_len=20; key_len=20; fn=MAC.hmac_sha1}
-    | SHA1_96 -> { name=SHA1_96; digest_len=12; key_len=20; fn=MAC.hmac_sha1}
-    | MD5 -> { name=MD5; digest_len=16; key_len=16; fn=MAC.hmac_md5 }
-    | MD5_96 -> { name=MD5_96; digest_len=12; key_len=16; fn=MAC.hmac_md5 }
-    | None -> { name=None; digest_len=0; key_len=0; fn=null_mac }
+  let null_mac (_:string) = object
+    method add_byte (x:int) = ()
+    method add_char (x:char) = ()
+    method add_string (x:string) = ()
+    method add_substring (x:string) (_:int) (_:int) = ()
+    method hash_size = 0
+    method result = ""
+    method wipe = () 
+  end
 
-    (* Given an algorithm, sequence number, initialisation vector and some data,
-       generate a hash and return it *)
-    let generate mac seqnum key src off len =
-        let hash = mac.fn key in
-        let i32 = !seqnum in
-        let i32s = String.create 4 in
-        let (&&) = Int32.logand in let (>>|) = Int32.shift_right_logical in
-        i32s.[3] <- Char.chr (Int32.to_int (i32 && 255l));
-        i32s.[2] <- Char.chr (Int32.to_int ((i32 >>| 8) && 255l));
-        i32s.[1] <- Char.chr (Int32.to_int ((i32 >>| 16) && 255l));
-        i32s.[0] <- Char.chr (Int32.to_int ((i32 >>| 24) && 255l));
-        hash#add_string i32s;
-        hash#add_substring src off len;
-        let mk = hash#result in
-        match mac.name with
-        | SHA1 | MD5 -> mk
-        | SHA1_96 | MD5_96 -> String.sub mk 0 mac.digest_len
-        | None -> ""
+  let info = function
+  | SHA1 -> { name=SHA1; digest_len=20; key_len=20; fn=MAC.hmac_sha1}
+  | SHA1_96 -> { name=SHA1_96; digest_len=12; key_len=20; fn=MAC.hmac_sha1}
+  | MD5 -> { name=MD5; digest_len=16; key_len=16; fn=MAC.hmac_md5 }
+  | MD5_96 -> { name=MD5_96; digest_len=12; key_len=16; fn=MAC.hmac_md5 }
+  | None -> { name=None; digest_len=0; key_len=0; fn=null_mac }
+
+  (* Given an algorithm, sequence number, initialisation vector and some data,
+     generate a hash and return it *)
+  let generate mac seqnum key src off len =
+    let hash = mac.fn key in
+    let i32 = !seqnum in
+    let i32s = String.create 4 in
+    let (&&) = Int32.logand in let (>>|) = Int32.shift_right_logical in
+    i32s.[3] <- Char.chr (Int32.to_int (i32 && 255l));
+    i32s.[2] <- Char.chr (Int32.to_int ((i32 >>| 8) && 255l));
+    i32s.[1] <- Char.chr (Int32.to_int ((i32 >>| 16) && 255l));
+    i32s.[0] <- Char.chr (Int32.to_int ((i32 >>| 24) && 255l));
+    hash#add_string i32s;
+    hash#add_substring src off len;
+    let mk = hash#result in
+    match mac.name with
+    | SHA1 | MD5 -> mk
+    | SHA1_96 | MD5_96 -> String.sub mk 0 mac.digest_len
+    | None -> ""
 
 end
