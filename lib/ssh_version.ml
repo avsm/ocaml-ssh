73e28f7f955b9f1ae34ca7559de81e61d2c0a0b7
Anil Madhavapeddy <anil@recoil.org>
2012-03-03 15:47:17 +0000

    whitespace in Version

--- lib/ssh_version.ml
+++ lib/ssh_version.ml
@@ -1,4 +1,3 @@
-(*pp cpp *)
 (*
  * Copyright (c) 2006 Anil Madhavapeddy <anil@recoil.org>
  *
@@ -19,47 +18,46 @@
 open Printf
 open Ssh_utils
 
-type t = { raw: string option;   (* needed for KEXDH_REPLY signature hash *)
-           protoversion: string;
-           softwareversion: string }
+type t = {
+  raw: string option;  (* needed for KEXDH_REPLY signature hash *)
+  protoversion: string;
+  softwareversion: string;
+}
            
 let to_string p =
-    match p.raw with
-    |None -> sprintf "SSH-%s-%s" p.protoversion p.softwareversion
-    |Some x -> x
+  match p.raw with
+  |None -> sprintf "SSH-%s-%s" p.protoversion p.softwareversion
+  |Some x -> x
 
 exception Parse_failure
 
 (* Read version character by character.  Cant use channels due to buffering *)
 let read_version_line ofd =
-	let buf = String.create 256 in
-	let ver = ref "" in
-	let off = ref 0 in
-	let got_nl = ref false in
-	while not (!got_nl) do
-		ofd#read buf !off 1;
-		match String.get buf !off with
-		|'\n' ->
-			(* newline is here, so stop reading *)
-			got_nl := true;
-			ver := String.sub buf 0 !off
-		|'\r' ->
-			(* just discard this, as it will be followed by a newline *)
-			()
-		|_ -> incr off
-	done;
-	!ver
-	
+  let buf = String.create 256 in
+  let ver = ref "" in
+  let off = ref 0 in
+  let got_nl = ref false in
+  while not (!got_nl) do
+    ofd#read buf !off 1;
+    match String.get buf !off with
+    |'\n' ->
+      (* newline is here, so stop reading *)
+      got_nl := true;
+      ver := String.sub buf 0 !off
+    |'\r' ->
+      (* just discard this, as it will be followed by a newline *)
+      ()
+    |_ -> incr off
+  done;
+  !ver
+  
 let unmarshal ofd = 
-    let str = read_version_line ofd in
-	prerr_endline (sprintf "Remote version is: %s" str);
-    (* It should be in form SSH-protoversion-softwareversioncomments    *)
-    if String.sub str 0 4 <> "SSH-" 
-    then raise Parse_failure;
-    let str' = String.sub str 4 (String.length str - 4) in
-    let other_dash =
-        try String.index str' '-' 
-        with Not_found -> raise Parse_failure in
-    let proto = String.sub str' 0 (other_dash - 1) in
-    let software = String.sub str' (other_dash + 1) (String.length str' - other_dash - 1) in
-    { raw = Some str; protoversion = proto; softwareversion = software }
+  let str = read_version_line ofd in
+  prerr_endline (sprintf "Remote version is: %s" str);
+  (* It should be in form SSH-protoversion-softwareversioncomments    *)
+  if String.sub str 0 4 <> "SSH-" then raise Parse_failure;
+  let str' = String.sub str 4 (String.length str - 4) in
+  let other_dash = try String.index str' '-' with Not_found -> raise Parse_failure in
+  let proto = String.sub str' 0 (other_dash - 1) in
+  let software = String.sub str' (other_dash + 1) (String.length str' - other_dash - 1) in
+  { raw = Some str; protoversion = proto; softwareversion = software }
